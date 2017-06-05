cea167a8ccaeba31ef8c89a18d89a184694b6b6d
Josh Vickerson <jvickerson@clickspark.com>
2017-06-05 19:17:39 -0400

    make it retweet, fix sample config

--- config-sample.js
+++ config-sample.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "consumer_secret": "consumer_secret",
-    "consumer_key": "consumer_key",
+  "consumer_secret": "consumer_secret",
+  "consumer_key": "consumer_key",
 	"access_token": "access_token",
-    "access_token_secret": "token_secret"
-  }
+  "access_token_secret": "token_secret"
+}
--- index.js
+++ index.js
@@ -2,27 +2,39 @@
     IMPORTS
 */
 var Twit = require('twit')
-var tokens = require('../config')
+var tokens = require('./config')
 
 /*
     Create the Twit object
 */
 var twitter = new Twit(tokens)
 
+// array to hold tweet ids to retweet
+var tweets = [];
+
 // listen for the tweets
 var stream = twitter.stream('statuses/filter', { track: '#ThingsMyCoworkersSay', language: 'en' })
 
 stream.on('tweet', function (tweet) {
   // add tweet to retweet queue
-  // maybe do some additional filtering?
-  // this gives control over the pace of how often tweets are retweeted and will prevent the bot from
-  // being flagged as spam (hopefully)
+  tweets.push(tweet.id_str);
 })
 
-stream.on('limit', function (limitMessage) {
-  // notify me that the bot is limited
-})
+// some placeholders for possible future fancy features
+// stream.on('limit', function (limitMessage) {
+//   // notify me that the bot is limited?
+// })
+//
+// stream.on('reconnect', function (request, response, connectInterval) {
+//   // notify me the bot is trying to reconnect?
+// })
 
-stream.on('reconnect', function (request, response, connectInterval) {
-  // notify me the bot is trying to reconnect?
-})
+// every 10 minutes, check if there's a tweet to retweet
+setInterval(function() {
+  if(tweets.length > 0) {
+    var tweet_id = tweets.shift(); // grab the first tweet id
+    twitter.post('statuses/retweet/:id', { id: tweet_id }, function (err, data, response) {
+      // do nothing, I guess
+    })
+  }
+}, 600000);
